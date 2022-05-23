8f8fb3d4928066982d65fc9a3dcfb1315091eddb
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-05-23 18:19:28 +0200

    20220523

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ const App = () => {
   
   const [datas, setData] = useState("")
   const [isPending, setIsPending] =useState(false)
+  const [error, setError] =useState(false)
 
   const getWeather =  (event) => {
 
     event.preventDefault()
     const city = event.target.elements.city.value
-
    
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
               .then(res =>{
@@ -26,12 +26,13 @@ const App = () => {
                       setData(data)
                       setIsPending(false)
                     }, 2000)
-              })  
+              }) 
   }
 
   return(
     <div className='App'>
       <Form loadWeather={getWeather}/>
+      {error && <div>City not found...</div>}
       {isPending && <div>Loading...</div>}
       {datas && <Weather name={datas}/>}
     </div>
